990d040cb8f3c07c2af8a2b80b709ec98bd111a9
rango <1614056450@qq.com>
2019-06-06 14:59:55 +0800

    测试文件上传

--- modbus_view/urls.py
+++ modbus_view/urls.py
@@ -7,4 +7,5 @@ app_name = 'modbus_view'
 urlpatterns = [
 path('index/',index_view.as_view(),name='index'),
 path('modbus_method_01/',modbus_method_01.as_view(),name='modbus_method_01'),
+path('upload_file/',upload_file,name='upload_file'),
 ]
--- modbus_view/views.py
+++ modbus_view/views.py
@@ -1,7 +1,9 @@
+import os
+import json
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
-
+from .forms import UploadFileForm
 # Create your views here.
 
 class index_view(View):
@@ -11,3 +13,29 @@ class index_view(View):
     def get(self,request):
         msg = "nihao"
         return render(request, 'pc/index.html', {"msg": msg})
+
+def handle_uploaded_file(f):
+    if os.path.exists('uploads/'+f.name):
+        return False
+    else:
+        with open('uploads/'+f.name, 'ab+') as destination:
+            for chunk in f.chunks():
+                # destination.write(chunk.decode('utf-8'))
+                destination.write(chunk)
+        return True
+
+def upload_file(request):
+    data={'name':''}
+    if request.method == 'GET':
+        form = UploadFileForm()
+        return render(request, 'pc/upload.html', {'form': form})
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if not handle_uploaded_file(request.FILES['docfile']):
+            data["name"] = "false"
+            return HttpResponse(json.dumps(data))
+        data["name"] = "true"
+        return HttpResponse(json.dumps(data))
+    else:
+        form = UploadFileForm()
+    return render(request, 'pc/upload.html', {'form': form})
